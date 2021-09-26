1d7ea8d30a3d750f1693232a71e12a8a3d3b5140
umanggoel2001 <umang.1923it1013@kiet.edu>
2021-09-26 23:51:55 +0530

    working with express middlewares

--- day7/index.js
+++ day7/index.js
@@ -1,16 +1,21 @@
 const express=require('express');
 const app=express();
 
-app.get('/',(req,res)=>{
-    res.send("hello");
-})
+app.get('/',(req,res,next)=>{
+    console.log("in first");
+    next();},(req,res)=>{
+        res.status(200);
+        res.json(req.query);
+    }
+  
+)
 
-app.get('/user/:id',(req,res) => {
-    res.send(req.params.id)
-})
+// app.get('/user/:id',(req,res) => {
+//     res.send(req.params.id)
+// })
 
-app.get('/feed?limit=20', (req, res) => {
-    res.send(req.query.limit)
-})
+// app.get('/feed?limit=20', (req, res) => {
+//     res.send(req.query.limit)
+// })
 //defining the port
 app.listen(5000);
